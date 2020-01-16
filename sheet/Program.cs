acf6ffa71d63563b0878fcd67ee734f24bfb4980
Ryan Hake <ryan.hake@gmail.com>
2020-01-16 13:00:31 -0600

    Updated program description comment

--- sheet/Program.cs
+++ sheet/Program.cs
@@ -1,4 +1,26 @@
-﻿using System;
+/* Program to read a purchase order and add information to our excel engineering
+ * schedule.  Also copies customer files over from quote folder to master customer
+ * file folder.
+ * Someday I will convert the schedule to use my own GUI with a sql server of some
+ * type.
+ *
+ * Copyright(C) 2018  Ryan S. Hake
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.If not, see<http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
